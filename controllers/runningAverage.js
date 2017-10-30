4faeff4e62a8a0977de968d71492948a17196977
Rahul Kadukar <rahulkadukar@yahoo.com>
2017-10-29 23:20:48 -0400

    Initial commit

---
+++ controllers/runningAverage.js
@@ -0,0 +1,180 @@
+const { Client } = require('pg');
+const promise = require('promise');
+const dbConnection = require('./privateSettings');
+
+const pgsql = new Client({
+  user: dbConnection.user,
+  host: dbConnection.host,
+  database: dbConnection.database,
+  password: dbConnection.password,
+  port: dbConnection.port
+})
+
+let totalTickers;
+let batchSize = 200;
+var t0;
+
+function parsePostgresOutput(pgsqlData) {
+  let str = JSON.stringify(pgsqlData);
+  let output = JSON.parse(str);
+  return output.rows;
+}
+
+pgsql.connect()
+initializeCalculations();
+
+function initializeCalculations() {
+  let countTickerQuery = 'SELECT COUNT(*) FROM ' +
+  '(SELECT DISTINCT ticker FROM "stocks"."stockData") AS temp';
+
+  pgsql.query(countTickerQuery, (err, result) => {
+    if (err) {
+      console.log("Issue in determining count of tickers");
+    } else {
+      totalTickers = parsePostgresOutput(result);
+      console.log(totalTickers[0].count);
+      t0 = (new Date).getTime();
+      startDataFetch(0);
+    }
+  })
+}
+
+function startDataFetch(e) {
+  if (e > totalTickers) {
+    process.exit();
+    return;
+  } else {
+    fetchData(e).then(() => {
+      startDataFetch(e + batchSize);
+    });
+  }
+}
+
+function fetchData(e) {
+  return new Promise((resolve, reject) => {
+    let fetchQuery = 'SELECT DISTINCT "ticker" FROM "stocks"."stockData" OFFSET ' +
+      e + ' LIMIT ' + batchSize;
+    
+    pgsql.query(fetchQuery, (err, result) => {
+      if (err) {
+        console.log("Error in fetching data for OFFSET " + e);
+        resolve();
+      } else {
+        let oData = parsePostgresOutput(result);
+        let promises = [];
+
+        for (let x in oData) {
+          promises.push(calculateRunningAverage(oData[x].ticker));
+        }
+        
+        Promise.all(promises).then(() => {
+          let z = (new Date).getTime() - t0;    
+          console.log(oData[0].ticker + ' to ' + oData[oData.length - 1].ticker + ' took ' + z + ' µs');
+          t0 = (new Date).getTime();
+          resolve();
+        });
+      }
+    });
+  });
+}
+
+function calculateRunningAverage(ticker) {
+  return new Promise((resolve, reject) => {
+    let movingAvg = {};    
+    let stockData = [];
+    let fetchQuery = 'SELECT * FROM "stocks"."stockData" WHERE "ticker" = \'' +
+      ticker + '\' ORDER BY "tickerDate" ASC';
+    
+    pgsql.query(fetchQuery, (err, result) => {
+      if (err) {
+        console.log("Error in fetching data for ticker " + ticker);
+        resolve();
+      } else {
+        let oData = parsePostgresOutput(result);
+        let promises = [];
+
+        for (let x in oData) {
+          stockData.push(oData[x]);
+        }
+
+        let intervals = [50, 100, 200];
+
+        for (let a in intervals) {
+          let start = 0;
+          let stop = 0;
+          let openAvg = 0.0000;
+          let duration = 'm' + intervals[a];
+          movingAvg[duration] = [];
+          
+          let mover = intervals[a];
+    
+          for (let y in stockData) {
+            if (stop - start === mover) {
+              start++;
+              stop++;
+              let avgRecord = {};
+              avgRecord.avgDate = stockData[y].tickerDate.slice(0, 10);
+              avgRecord.price = ((openAvg / mover).toFixed(2));
+              movingAvg[duration].push(avgRecord);
+              openAvg -= (stockData[y - mover].closePrice + stockData[y - mover].openPrice) / 2;
+            } else {
+              stop++;
+            }
+            openAvg = openAvg + ((stockData[y].closePrice + stockData[y].openPrice) / 2);
+          }        
+        }
+
+        let x = 0;
+        let maxLength = 12;
+
+        for (let a = 0; a < maxLength; ++a) {
+          promises.push(insertDataIntoTable(x, ticker, movingAvg));
+          x++;
+        }
+
+        Promise.all(promises).then(() => {
+          resolve();
+        });
+      }
+    });
+  });
+}
+
+function insertDataIntoTable(i, ticker, movingAvg) {
+  let counter = 0;
+  let queryBreak = 500;
+  let initQuery = 'INSERT INTO "stocks"."runningAvg" ' + 
+    '("ticker", "tickerDate", "avgType", "fields", "price") VALUES ';
+  let rawQuery = initQuery;
+
+  let beginCount = i * queryBreak;
+  let endCount = beginCount + queryBreak;
+
+  return new Promise((resolve, reject) => {
+    for (let a in movingAvg) {
+      while ((beginCount < endCount)) {
+        if (movingAvg[a].length < (beginCount + 1)) {
+          break;
+        }
+  
+        rawQuery += '(\'' + ticker + '\', \'' + movingAvg[a][beginCount].avgDate + '\', \'' + a +
+        '\', \'OC\', \'' + movingAvg[a][beginCount].price + '\'), '; 
+        beginCount++;
+        counter++;
+      }
+      beginCount = i * queryBreak;
+    }
+  
+    if (rawQuery === initQuery) {
+      resolve();
+    } else {
+      pgsql.query(rawQuery.slice(0, -2), (err, result) => {
+        if (err) {
+          console.log("Error in inserting data for ticker");
+          console.log(err);
+        }
+        resolve();
+      })
+    }
+  });
+}
---
+++ controllers/validator.js
@@ -0,0 +1,55 @@
+const { Client } = require('pg');
+const promise = require('promise');
+const dbConnection = require('./privateSettings');
+
+const pgsql = new Client({
+  user: dbConnection.user,
+  host: dbConnection.host,
+  database: dbConnection.database,
+  password: dbConnection.password,
+  port: dbConnection.port
+})
+
+pgsql.connect()
+initializeCalculations();
+
+function parsePostgresOutput(pgsqlData) {
+  let str = JSON.stringify(pgsqlData);
+  let output = JSON.parse(str);
+  return output.rows;
+}
+
+var t0 = (new Date).getTime();
+var stockData = [];
+let totalCount = 0;
+
+function initializeCalculations() {
+  var sql = 'SELECT ticker, COUNT(*) AS total FROM "stocks"."stockData" GROUP BY ticker';
+  
+  pgsql.query(sql, function (err, rows) {
+    if (err) { 
+      
+    } else {
+      let result = parsePostgresOutput(rows);
+
+      for (var x in result) { 
+        stockData.push(result[x]);
+      }
+
+      for (let x in stockData) {
+        let y = (stockData[x].total);
+        let dynamic = [50, 100, 200];
+
+        for (let a in dynamic) {
+          let z = y - dynamic[a];
+          if (z > 0) {
+            totalCount += z;
+          }
+        }
+      }
+
+      console.log(totalCount);
+      console.log((new Date).getTime() - t0 + " microseconds");
+    }
+  });
+}
